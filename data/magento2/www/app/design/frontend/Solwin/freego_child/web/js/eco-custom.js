56d2454c0457b285b8f945bc5ec50256204d2437
hotcustard <support@hotcustard.co.uk>
2022-03-15 08:59:31 +0000

    pre nav work

--- data/magento2/www/app/design/frontend/Solwin/freego_child/web/js/eco-custom.js
+++ data/magento2/www/app/design/frontend/Solwin/freego_child/web/js/eco-custom.js
@@ -137,83 +137,83 @@ jQuery('table').wrap('<div class="table-responsive"></div>');
 
 
 
-require(['jquery', 'cpowlcarousel'], function ($) {
-    $(document).ready(function () {
-        $(".hb-slider").owlCarousel({
-            items: 1,
-			loop:true,
-            itemsDesktop: [1080, 1],
-            itemsDesktopSmall: [860, 1],
-            itemsTablet: [768, 1],
-            itemsTabletSmall: [639, 1],
-            itemsMobile: [360, 1],
-            pagination: true,
-            navigationText: ["<i class='fa fa-angle-left'></i>", "<i class='fa fa-angle-right'>"],
-            navigation: false,
-			autoPlay : 5000,
+// require(['jquery', 'cpowlcarousel'], function ($) {
+//     $(document).ready(function () {
+//         $(".hb-slider").owlCarousel({
+//             items: 1,
+// 			loop:true,
+//             itemsDesktop: [1080, 1],
+//             itemsDesktopSmall: [860, 1],
+//             itemsTablet: [768, 1],
+//             itemsTabletSmall: [639, 1],
+//             itemsMobile: [360, 1],
+//             pagination: true,
+//             navigationText: ["<i class='fa fa-angle-left'></i>", "<i class='fa fa-angle-right'>"],
+//             navigation: false,
+// 			autoPlay : 5000,
 
-        });
+//         });
 
-        $(".hpb-slider").owlCarousel({
-            items: 3,
-            itemsDesktop: [1080, 3],
-            itemsDesktopSmall: [860, 3],
-            itemsTablet: [768, 2],
-            itemsTabletSmall: [639, 2],
-            itemsMobile: [479, 1],
-            pagination: false,
-            navigationText: ['<div class="lft-btn"><i class="fa fa-angle-left"></i></div>', '<div class="rgt-btn"><i class="fa fa-angle-right"></div>'],
-            navigation: true,
-        });
-        $(".htb-slider").owlCarousel({
-            items: 1,
-            itemsDesktop: [1080, 1],
-            itemsDesktopSmall: [860, 1],
-            itemsTablet: [768, 1],
-            itemsTabletSmall: [639, 1],
-            itemsMobile: [479, 1],
-            pagination: false,
-            navigationText: ['<div class="lft-btn"><i class="fa fa-angle-left"></i></div>', '<div class="rgt-btn"><i class="fa fa-angle-right"></div>'],
-            navigation: true,
-        });
-        $(".hbr-slider").owlCarousel({
-            items:4,
-			 margin: 20,
-            itemsDesktop: [1080, 4],
-            itemsDesktopSmall: [860, 3],
-            itemsTablet: [768, 3],
-            itemsTabletSmall: [639, 3],
-            itemsMobile: [479, 2],
-              pagination: false,
+//         $(".hpb-slider").owlCarousel({
+//             items: 3,
+//             itemsDesktop: [1080, 3],
+//             itemsDesktopSmall: [860, 3],
+//             itemsTablet: [768, 2],
+//             itemsTabletSmall: [639, 2],
+//             itemsMobile: [479, 1],
+//             pagination: false,
+//             navigationText: ['<div class="lft-btn"><i class="fa fa-angle-left"></i></div>', '<div class="rgt-btn"><i class="fa fa-angle-right"></div>'],
+//             navigation: true,
+//         });
+//         $(".htb-slider").owlCarousel({
+//             items: 1,
+//             itemsDesktop: [1080, 1],
+//             itemsDesktopSmall: [860, 1],
+//             itemsTablet: [768, 1],
+//             itemsTabletSmall: [639, 1],
+//             itemsMobile: [479, 1],
+//             pagination: false,
+//             navigationText: ['<div class="lft-btn"><i class="fa fa-angle-left"></i></div>', '<div class="rgt-btn"><i class="fa fa-angle-right"></div>'],
+//             navigation: true,
+//         });
+//         $(".hbr-slider").owlCarousel({
+//             items:4,
+// 			 margin: 20,
+//             itemsDesktop: [1080, 4],
+//             itemsDesktopSmall: [860, 3],
+//             itemsTablet: [768, 3],
+//             itemsTabletSmall: [639, 3],
+//             itemsMobile: [479, 2],
+//               pagination: false,
 			
-            navigationText: ['<div class="lft-btn"><i class="fa fa-angle-left"></i></div>', '<div class="rgt-btn"><i class="fa fa-angle-right"></div>'],
-            navigation: true,
-        });
+//             navigationText: ['<div class="lft-btn"><i class="fa fa-angle-left"></i></div>', '<div class="rgt-btn"><i class="fa fa-angle-right"></div>'],
+//             navigation: true,
+//         });
 		
-		 $(".homelatestprd .products-row").owlCarousel({
-            items:4,
-			 margin: 20,
-            itemsDesktop: [1080, 4],
-            itemsDesktopSmall: [860, 3],
-            itemsTablet: [768, 3],
-            itemsTabletSmall: [639, 3],
-            itemsMobile: [479, 2],
-              pagination: false,
+// 		 $(".homelatestprd .products-row").owlCarousel({
+//             items:4,
+// 			 margin: 20,
+//             itemsDesktop: [1080, 4],
+//             itemsDesktopSmall: [860, 3],
+//             itemsTablet: [768, 3],
+//             itemsTabletSmall: [639, 3],
+//             itemsMobile: [479, 2],
+//               pagination: false,
 			
-            navigationText: ['<div class="lft-btn"><i class="fa fa-angle-left"></i></div>', '<div class="rgt-btn"><i class="fa fa-angle-right"></div>'],
-            navigation: true,
-        });
+//             navigationText: ['<div class="lft-btn"><i class="fa fa-angle-left"></i></div>', '<div class="rgt-btn"><i class="fa fa-angle-right"></div>'],
+//             navigation: true,
+//         });
 		
 		
 		
 	
 		
-    });
+//     });
 	
 	
 		
 	
 	
 	
-});
+// });
 
